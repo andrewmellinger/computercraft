b7599fafbd0e55d320f3ed6bd13a7ccfcb6f4757
Andrew Mellinger <aomellinger@sei.cmu.edu>
2014-03-13 14:04:53 -0400

    Added in ccts work.

--- rom/apis/turtle/crush.lua
+++ rom/apis/turtle/crush.lua
@@ -5,6 +5,7 @@
 REFUEL_LEVEL = 15
 REFULE_SLOT = 1
 
+-- Used to reload fuel if needed
 function checkFuel()
   if turtle.getFuelLevel() < REFUEL_LEVEL then
     turtle.select(REFUEL_SLOT)
@@ -12,15 +13,25 @@ function checkFuel()
   end
 end
 
+-- Digs forward until we don't detect anything.
+-- Good for getting through sand/gravel.
 function digAll()
   while turtle.detect() do
     turtle.dig()
   end
 end
 
+-- Digs forward until we don't detect anything.
+-- Good for getting through sand/gravel.
+function digAllUp()
+  while turtle.detectUp() do
+    turtle.digUp()
+  end
+end
 
--- Extract the entire forward column and move
--- into that column
+
+-- Extract up to a 3 block column ahead.  One up, one forward
+-- and one down.
 function digColumn(up, down)
   digAll()
   turtle.forward()
@@ -36,7 +47,8 @@ end
 
 
 -- Digs a full row (potentially up and down) executing fn
--- in each cell if not nil
+-- in each block if not nil.  The fn is good for placing torches.
+-- The function is passed a monotonically incrementing counter
 function digRow(length, up, down, fn)
   for i = 1,length do
     digColumn(up, down)
@@ -47,16 +59,22 @@ function digRow(length, up, down, fn)
 end
 
 
+-- Moves the turtle forward and executes the fn in each block as it moves.
+-- Each way to make a digger or torch placements.
+-- The function is passed a monotonically incrementing counter
 function fnOverRow(length, fn, counter)
   checkFuel()
   for i = 1,length do
     fn(counter)
+    digAll()
     turtle.forward()
     counter = counter + 1
   end
   return counter
 end
 
+
+-- Parameterized function to do turns.
 function turn(right)
   if right then
     turtle.turnRight()
@@ -65,8 +83,11 @@ function turn(right)
   end
 end
 
+
 -- Go out this many, turn right or left, go over (w+1), come back
--- Execute the fn BEFORE moving forward
+-- Note, the over is a number of blocks to move over.  If you 
+-- want it to come back on the same one it went out, specify -1.
+-- Execute the fn BEFORE moving forward.
 -- The function is passed a monotonically incrementing counter
 function outAndBack(length, width, right, fn)
   local counter = 1
@@ -76,8 +97,8 @@ function outAndBack(length, width, right, fn)
   counter = fnOverRow(width + 1, fn, counter)
   turn(right)
   counter = fnOverRow(length, fn, counter)
-
 end
+
 
 -- A functions that clears out and back.  
 -- This is what pumpkins does
@@ -85,7 +106,9 @@ function clearOutAndBack(length, width)
   outAndBack(length, width, true, function (x) turtle.dig() end)
 end
 
--- Usually used for placing torches
+
+-- Functions used to place torches on some interval.  Often passed
+-- in to the other movement loops.
 function placeDownWithInterval(idx, interval, slot)
   if idx % interval == 0 then
     turtle.select(torchSlot)
@@ -94,8 +117,177 @@ function placeDownWithInterval(idx, interval, slot)
 end
 
 
+-- ============================================================================
+-- ============================================================================
+--  ####  #### #####  #### 
+-- #     #       #   #     
+-- #     #       #    ###  
+-- #     #       #       # 
+--  ####  ####   #   #### 
 
--- ================================================
+-- TODO: Add entire ccts engine here
+
+
+-- The vocabulary primitives
+function selectAndPlace(invId)
+  turtle.select(invId)
+  turtle.placeDown()
+end
+
+
+-- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+function goForward(idx)
+  --if turtle.detect() then
+  --  turtle.dig()
+  --end
+  turtle.forward()
+  return idx
+end
+
+
+-- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+function repeatIt(idx)
+  -- Format:  @[0-9]*\[cmds\]
+  
+  -- Look for square bracked
+  local bracket = idx + 1
+  while cctx_context.cmds:sub(bracket, bracket) ~= '[' do
+    bracket = bracket + 1
+  end
+  
+  -- Numeric/count portion
+  local count = tonumber(cctx_context.cmds:sub(idx + 1, bracket - 1))
+
+  while count > 0 do
+    newIdx = bracket + 1
+    local cmd = cctx_context.cmds:sub(newIdx, newIdx)
+    while cmd ~= ']' do
+      newIdx = dispatch(cmd, newIdx)
+      newIdx = newIdx + 1
+      cmd = cctx_context.cmds:sub(newIdx, newIdx)
+    end
+    count = count - 1
+  end
+
+  -- This should be ending bracket
+  local tmp = cctx_context.cmds:sub(newIdx, newIdx)
+  if tmp ~= ']' then
+    print("RepeatIt is not returning on ']', instead: "..tmp)
+  end
+
+  return newIdx
+end
+
+
+-- Specific to CCTS
+local function placeFront(idx)
+  idx = idx + 1
+  local c = tonumber(cctx_context.cmds:sub(idx, idx))
+  turtle.select(c)
+  turtle.place()
+  return idx
+end
+
+
+-- Specific to CCTS
+local function placeUp(idx)
+  idx = idx + 1
+  local c = tonumber(cctx_context.cmds:sub(idx, idx))
+  turtle.select(c)
+  turtle.placeUp()
+  return idx
+end
+
+
+-- Specific to CCTS
+local function ignoreComments(idx)
+  while cctx_context.cmds:sub(idx, idx) ~= "\n" do
+    idx = idx + 1
+  end
+  return idx
+end
+
+
+-- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+-- Specific to CCTS
+-- TOOD:  Since we have IDX, do we really need sym?
+local function dispatch(idx)
+  local sym = cctx_context.cmds:sub(idx,idx)
+  func = cctx_context.funcs[sym]
+  if func == nil then
+    print("Failed to find function for: '" ..sym.."'")
+  else
+    idx = func(idx);
+  end
+  return idx
+end
+
+
+-- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+-- Main loop context
+
+ccts_context = {}
+
+-- Executes a series of commands embedded in this file.
+function ccts(cmds)
+
+  -- Construct jumptable
+  local funcs = {}
+  funcs['f'] = function(idx) goForward() return idx end
+  funcs['b'] = function(idx) turtle.back() return idx end
+  funcs['l'] = function(idx) turtle.turnLeft() return idx end
+  funcs['r'] = function(idx) turtle.turnRight() return idx end
+  funcs['u'] = function(idx) turtle.up() return idx end
+  funcs['d'] = function(idx) turtle.down() return idx end
+
+  funcs['*'] = function(idx) digAll() return idx end
+  funcs['/'] = function(idx) digAllUp() return idx end
+  funcs['\\'] = function(idx) turtle.digDown() return idx end
+
+  funcs['1'] = function(idx) selectAndPlace(1) return idx end
+  funcs['2'] = function(idx) selectAndPlace(2) return idx end
+  funcs['3'] = function(idx) selectAndPlace(3) return idx end
+  funcs['4'] = function(idx) selectAndPlace(4) return idx end
+  funcs['5'] = function(idx) selectAndPlace(5) return idx end
+  funcs['6'] = function(idx) selectAndPlace(6) return idx end
+  funcs['7'] = function(idx) selectAndPlace(7) return idx end
+  funcs['8'] = function(idx) selectAndPlace(8) return idx end
+  funcs['9'] = function(idx) selectAndPlace(9) return idx end
+  funcs['0'] = function(idx) selectAndPlace(10) return idx end
+
+  funcs['\n'] = function(idx) return idx end
+  funcs['.']  = function(idx) return idx end
+  funcs['']   = function(idx) return idx end
+  funcs['\r'] = function(idx) return idx end
+  funcs[' '] = function(idx) return idx end
+
+  funcs['@'] = repeatIt
+  funcs['~'] = placeFront
+  funcs['^'] = placeUp
+  funcs['#'] = ignoreComments
+
+  ccts_context['funcs'] = funcs
+  ccts_context['cmds'] = cmds
+
+  -- Execute the file one command at a time
+  local idx = 0
+  while idx <= #cmds do
+    checkFuel()
+    idx = dispatch(idx)
+    idx = idx + 1;
+  end
+
+end
+
+
+-- ============================================================================
+-- ============================================================================
 
 -- getopt_alt.lua
 
@@ -143,6 +335,8 @@ function getopt( arg, options )
   return tab
 end
 
+
+-- getopt does NOT strip switches.  This function does that, sort of...
 function stripSwitches(arg)
   local tab = {}
   for k,v in ipairs(arg) do
