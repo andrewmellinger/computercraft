799dbc7603d56bf9cb05037ca805f1813bbeb3e1
Andrew Mellinger <andrew.mellinger@gmail.com>
2014-03-28 07:45:35 -0400

    Reworked bridge to use command line args, support multiple stacks (i.e. go long than 64 if it has materials) and stop when it reaches a wall, or runs out of materials.

--- rom/programs/turtle/bridge.lua
+++ rom/programs/turtle/bridge.lua
@@ -1,41 +1,66 @@
-function checkFuel()
-  if turtle.getFuelLevel() < 3 then
-    turtle.select(1)
-    turtle.refuel(1)
-  end
-end
-
-
-function showHelp()
-  print("Usage: bridge <length>")
-  print("  slot 1: coal")
-  print("  slot 2: blocks for bridging")
-end
+--turtle = require "TurtleSim"
+--local crush = turtle.loadCrush()
+os.loadAPI("crush")
 
 
 function mainLoop(length)
   print("Starting bridge length: "..length)
-  torchit = 6
-  while (length > 0) do
-    checkFuel()
+
+  -- Length can be positive (max) or negative which
+  -- is go until wall our out of blocks.
+  while length ~= 0 do
+    crush.checkFuel()
+
+    -- If we run into something, we are done.
+    if turtle.detect() then
+      break
+    end
+
+    -- Look for more items if we have them
+    if turtle.getItemCount(2) == 1 then
+      crush.gatherItems(2, 3, 16)
+    end
+
+    -- Do the real work
     turtle.forward()
     if not turtle.detectDown() then
       turtle.select(2)
       turtle.placeDown()
     end
     length = length - 1
+
+    -- If we are out of supplies (we tried gathering above) 
+    -- then we are done.
+    if turtle.getItemCount(2) == 0 then
+      break
+    end
+
   end
 end
 
--- Main
 
-local tArgs = {...}
-if #tArgs < 1
+-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+function showHelp()
+  print("bridge <-l #>")
+  print(" -l #: Len, -1 go until wall/")
+  print("          out of blocks, def -1")
+  print(" s1: coal")
+  print(" s2+: blocks for bridging")
+end
+
+-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+-- Main
+local argTable = { l=-1, h=false }
+crush.overlayArgs(":l:h", argTable, ...)
+
+if argTable.h then
   showHelp()
 else
-  local length = tonumber( tArgs[1] )
-  mainLoop(length)
+  mainLoop(argTable.l)
 end
+
 print("Done")
 
 
